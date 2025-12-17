f665559419370c868bb9ae7324bb4ac74686f880
IhsanFackbo <159555124+IhsanFackbo@users.noreply.github.com>
2025-12-18 00:18:38 +0700

    Create Hi.js

---
+++ plugins/Hi.js
@@ -0,0 +1,10 @@
+const handler = async () => {
+  // isi scrape kamu di sini
+};
+
+handler.alias = "Example Plugin";
+handler.category = "scrape";
+handler.tags = ["scrape"];
+handler.desc = "Contoh plugin";
+
+module.exports = handler;
