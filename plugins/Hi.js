8ca65fe3199b902895593744498abdda00665998
IhsanFackbo <159555124+IhsanFackbo@users.noreply.github.com>
2025-12-18 22:18:11 +0700

    Update Hi.js

--- plugins/Hi.js
+++ plugins/Hi.js
@@ -1,10 +1,36 @@
-const handler = async () => {
-  // isi scrape kamu di sini
-};
+const handler = async () => {};
 
-handler.alias = "Example Plugin";
+/* ======================
+   RAW CODE (DITAMPILKAN)
+   ====================== */
+handler.code = `
+// YouTube basic info scrape (contoh)
+const axios = require("axios");
+
+async function youtubeInfo(url) {
+  if (!url) throw new Error("URL wajib diisi");
+
+  const res = await axios.get(
+    "https://noembed.com/embed",
+    { params: { url } }
+  );
+
+  return {
+    title: res.data.title,
+    author: res.data.author_name,
+    thumbnail: res.data.thumbnail_url
+  };
+}
+
+module.exports = { youtubeInfo };
+`.trim();
+
+/* ======================
+   METADATA (KATALOG)
+   ====================== */
+handler.alias = "YouTube Scraper";
 handler.category = "scrape";
-handler.tags = ["scrape"];
-handler.desc = "Contoh plugin";
+handler.tags = ["youtube", "video"];
+handler.desc = "Ambil judul, author, dan thumbnail video YouTube.";
 
 module.exports = handler;
